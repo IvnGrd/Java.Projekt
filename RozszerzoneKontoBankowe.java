04a1698d9ad905c169f9fffa5d4b632258605463
IvnGrd <133670407+IvnGrd@users.noreply.github.com>
2023-05-29 16:32:01 +0200

    Add files via upload

---
+++ RozszerzoneKontoBankowe.java
@@ -0,0 +1,6 @@
+public class RozszerzoneKontoBankowe extends SwojeKontoBankowe {
+
+    public RozszerzoneKontoBankowe(String numerKonta, double saldo, Klient właściciel) {
+        super(numerKonta, saldo, właściciel);
+    }
+}
